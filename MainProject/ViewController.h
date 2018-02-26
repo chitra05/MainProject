38847e6d7f28404a6cad88218c435852198b1bd1
Chitra <chitrait.bonik@gmail.com>
2018-02-26 14:29:20 +0600

    Initial Commit

---
+++ MainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MainProject
+//
+//  Created by chitra on 2/26/18.
+//  Copyright © 2018 chitra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MainProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MainProject
+//
+//  Created by chitra on 2/26/18.
+//  Copyright © 2018 chitra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
